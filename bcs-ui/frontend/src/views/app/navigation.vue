69e8c1311c933a455da8d6fa83b9f1be34b46678
carlchen2 <carlchen2@canway.net>
2026-02-27 11:45:14 +0800

    feat: bcs-ui 支持隐藏部署管理

--- bcs-ui/frontend/src/views/app/navigation.vue
+++ bcs-ui/frontend/src/views/app/navigation.vue
@@ -190,7 +190,7 @@ export default defineComponent({
       });
     };
 
-    const { menusData: menus, leafMenus } = useMenu();
+    const { menus, leafMenus } = useMenu();
     const { config } = usePlatform();
     const appLogo = computed(() => config.appLogo || logoSvg);
     const appName = computed(() => config.i18n.productName);
