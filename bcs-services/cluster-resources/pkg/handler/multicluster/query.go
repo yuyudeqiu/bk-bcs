eae4a9bc51cf20586b4bd47e068856de7501a1b1
carlchen2 <carlchen2@canway.net>
2026-08-04 19:29:02 +0800

    fix(cluster-resources): handle API resources without uid

--- bcs-services/cluster-resources/pkg/handler/multicluster/query.go
+++ bcs-services/cluster-resources/pkg/handler/multicluster/query.go
@@ -498,7 +498,7 @@ func buildList(ctx context.Context, resources []*storage.Resource) map[string]in
 	manifestItems := []interface{}{}
 	// 获取 apiVersion
 	apiVersion := mapx.GetStr(resources[0].Data, "apiVersion")
-	kind := resources[0].ResourceType
+	kind := mapx.GetStr(resources[0].Data, "kind")
 	formatFunc := formatter.GetFormatFunc(kind, apiVersion)
 	pruneFunc := formatter.GetPruneFunc(kind)
 	// 遍历列表中的每个资源，生成 manifestExt
@@ -514,8 +514,11 @@ func buildList(ctx context.Context, resources []*storage.Resource) map[string]in
 			continue
 		}
 		ext["clusterID"] = item.ClusterID
-		manifestExt[uid.(string)] = ext
-		manifestItems = append(manifestItems, pruneFunc(item.Data))
+		// 过滤掉不支持uid的资源
+		if uidStr, ok := uid.(string); ok {
+			manifestExt[uidStr] = ext
+			manifestItems = append(manifestItems, pruneFunc(item.Data))
+		}
 	}
 	manifest["items"] = manifestItems
 	return map[string]interface{}{"manifest": manifest, "manifestExt": manifestExt}
